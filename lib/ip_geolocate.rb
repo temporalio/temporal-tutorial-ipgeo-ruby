b8b299c5ce657c629951b5703a66810a10cc999b
angelazhou32 <angela.zhou@temporal.io>
2025-05-16 10:35:18 -0400

    change snipsync

--- lib/ip_geolocate.rb
+++ lib/ip_geolocate.rb
@@ -1,9 +1,9 @@
+# @@@SNIPSTART ruby-ipgeo-shared
 # Load Bundler and load all your gems
 require_relative "ip_geolocate/get_ip_activity"
 require_relative "ip_geolocate/get_location_activity"
 require_relative "ip_geolocate/get_address_from_ip_workflow"
 
-# @@@SNIPSTART ruby-ipgeo-shared
 module IPGeolocate
   TASK_QUEUE_NAME = "ip-address-ruby"
 end
